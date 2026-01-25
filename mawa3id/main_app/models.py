d324f2e04a97cd09c3d95776935318c0aa49141c
AliFahmi07 <ali.fahmi.a12@gmail.com>
2026-01-25 18:45:50 +0300

    Profile Model

--- mawa3id/main_app/models.py
+++ mawa3id/main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    class Role(models.TextChoices):
+        CLIENT = "client", "Client"
+        BUSINESS_OWNER = "business_owner", "Business Owner"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE),
+    image = image = models.ImageField(upload_to="main_app/static/uploads", default="")
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.CLIENT,)
+
+    def __str__(self):
+        return f"{self.user} ({self.role})"
