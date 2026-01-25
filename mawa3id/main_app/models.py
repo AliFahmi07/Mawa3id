9df67df2a222ce1843e11f9f61713e1a1c6104b4
AliFahmi07 <ali.fahmi.a12@gmail.com>
2026-01-25 18:57:40 +0300

    Business Model

--- mawa3id/main_app/models.py
+++ mawa3id/main_app/models.py
@@ -8,9 +8,33 @@ class Profile(models.Model):
         CLIENT = "client", "Client"
         BUSINESS_OWNER = "business_owner", "Business Owner"
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE),
-    image = image = models.ImageField(upload_to="main_app/static/uploads", default="")
-    role = models.CharField(max_length=20, choices=Role.choices, default=Role.CLIENT,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = image = models.ImageField(upload_to="main_app/static/uploads"
+        default=""
+        )
+    role = models.CharField(
+        choices=Role.choices,
+        default=Role.CLIENT,
+        )
 
     def __str__(self):
         return f"{self.user} ({self.role})"
+
+class Business(models.Model):
+    class Category(models.TextChoices):
+        BARBER = "barber", "Barber"
+        DENTIST = "dentist", "Dentist"
+        GYM = "gym", "Gym"
+        SALON = "salon", "Salon"
+        CLINIC = "clinic", "Clinic"
+        OTHER = "other", "Other"
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="business")
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    category = models.CharField(
+        choices=Category.choices
+        default=Category.OTHER
+        )
+
+    def __str__(self):
+        return self.name
