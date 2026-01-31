446c66f5b81185786206c908c1cd261de7d6daf9
ammarys-w <ammar39876@gmail.com>
2026-01-31 18:17:30 +0300

    Add URL routes for auth, messaging, profiles, and businesses

--- mawa3id/main_app/urls.py
+++ mawa3id/main_app/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('business/create/', views.BusinessCreate.as_view(), name= 'create_business'),
     path('business/<int:user_id>', views.BusinessDetail.as_view(), name= 'business_detail'),
 
+    path('messages/', views.inbox, name='inbox'),
+    path('messages/<int:user_id>/', views.conversation, name='conversation'),
+    path('messages/send/<int:receiver_id>/', views.send_message, name='send_message'),
 
 ]
