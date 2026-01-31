8663e1e983f94af0ba8f36ddfcc23eb2d259b629
3bdulla03 <abdulla7545@gmail.com>
2026-01-31 14:50:07 +0300

    posts backend create post added

--- mawa3id/main_app/urls.py
+++ mawa3id/main_app/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     # POSTS
     path('posts/', views.posts_index, name='posts_index'),
     path('posts/<int:posts_id>/', views.posts_detail, name='posts_detail'),
+    path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
 
