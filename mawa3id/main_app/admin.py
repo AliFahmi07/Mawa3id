03d05e6138b73b4e09b2fc44bd0329a57d7c5abe
ammarys-w <ammar39876@gmail.com>
2026-01-31 18:34:53 +0300

    Register Messages in admin

--- mawa3id/main_app/admin.py
+++ mawa3id/main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Business
+from .models import Profile, Business, Messages
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Business)
+admin.site.register(Messages)
