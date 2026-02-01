417b3b6095b5f83ea77c19fdbcd20e781c3056e9
ammarys-w <ammar39876@gmail.com>
2026-02-01 15:23:58 +0300

    add service in admin to test the review

--- mawa3id/main_app/admin.py
+++ mawa3id/main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, Business, Review
+from .models import Profile, Business, Review, Service
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Business)
 admin.site.register(Review)
+admin.site.register(Service)
